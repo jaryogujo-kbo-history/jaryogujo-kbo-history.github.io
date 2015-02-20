a2d7dc938a26e656623cac347e5e1b63e8c93a9e
Inho Won <wonderino@gmail.com>
2015-02-20 23:27:38 +0900

    add data-manager

--- kbl-history.js
+++ kbl-history.js
@@ -1,3 +1,58 @@
-!(function() {
+d3.kblHistoryDataMng = function () {
+  var exports = {},
+    dispatch = d3.dispatch('dataReady', 'dataLoading'),
+    data;
 
-}())
+  exports.loadCsv = function(_path) {
+    var loadCsv = d3.csv(_path);
+    loadCsv.on('progress', function() {
+      dispatch.dataLoading(d3.event.loaded);
+    });
+
+    loadCsv.get(function(_err, _res) {
+      _res.forEach(function(d) {
+        //clean data!!
+        for (var k in d) {
+          if(d.hasOwnProperty(k) && !isNaN(d[k])) {
+            d[k] = +d[k]
+          } else if (k === 'coach_name') {
+            d[k] = d[k].split(',').map(function(d) {return d.trim()})
+          }
+        }
+
+      })
+      data = _res;
+      dispatch.dataReady(_res);
+    })
+
+  }
+
+  exports.data = function () {
+    return data;
+  };
+
+  d3.rebind(exports, dispatch, 'on');
+  return exports;
+}
+
+d3.kblHistory = function module () {
+  var attr = {
+    width : undefined
+  };//end of attributes
+
+  var exports = function (_selection) {
+    _selection.each(function(_data) {
+
+    }); //end of each
+  } // end of exports
+
+  function attrFunc(_) {
+    if(!arguments.length) {
+        return
+    }
+
+  }
+
+
+  return exports;
+}
