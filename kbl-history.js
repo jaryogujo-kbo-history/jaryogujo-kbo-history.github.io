7f2f032a796d860e381c0c0d4184f49ec3e93f19
Inho Won <wonderino@gmail.com>
2015-02-28 13:58:37 +0900

    add selection func

--- kbl-history.js
+++ kbl-history.js
@@ -46,9 +46,9 @@ d3.kblHistory = function module () {
   var x = d3.scale.ordinal(), y=d3.scale.ordinal(), xAxis = d3.svg.axis();
   var wa = d3.scale.linear();
   var teamMap = d3.map({'OB':'OB','SS':'삼성','MB':'MBC',
-  'HT':'해태','LT':'롯데','SM':'삼미','LG':'LG','DS':'두산','KA':'KIA',
-  'BG':'빙그레','HH':'한화','SK':'SK','NS':'넥센','NC':'NC','SB':'쌍방울',
-  'CB':'청보','TP':'태평양','HD':'현대'})
+    'HT':'해태','LT':'롯데','SM':'삼미','LG':'LG','DS':'두산','KA':'KIA',
+    'BG':'빙그레','HH':'한화','SK':'SK','NS':'넥센','NC':'NC','SB':'쌍방울',
+    'CB':'청보','TP':'태평양','HD':'현대'})
   var svg, curData;
   var exports = function (_selection) {
     _selection.each(function(_data) {
@@ -70,8 +70,6 @@ d3.kblHistory = function module () {
 
       //FIXME : get the scale of  winning rates
       xAxis.tickSize(0)
-      //.ticks(20)
-      //.tickValues(x.domain().filter(function(d) {return (d%2===0)}))
       .tickFormat(function(d) {
         return d3.format('02d')(d%100) + "'"
         //d3.format('d'))
@@ -129,21 +127,7 @@ d3.kblHistory = function module () {
     .enter().append('g')
     .attr('class', 'jg-col')
     .attr('transform', d3.svg.transform().translate(function(d) {return [x(d[0].year), 0]}))
-    .on('click', function(d,i) {
-      if (d3.select(this).classed('filtered')) {
-
-        col.classed({'filtered':false})
-      } else {
-        var thisCoach =d[0].first_coach_name
-        col.classed({'filtered':false})
-
-        col.filter(function(d) {
-              return d[0].first_coach_name === thisCoach
-          })
-          //.selectAll('rect.jg-col-block')
-          .classed({'filtered':true})
-      }
-    })
+    .on('click', selectCols)
 
     col.append('rect')
     .attr('class', 'jg-col-block')
@@ -153,16 +137,7 @@ d3.kblHistory = function module () {
       return d.length * x.rangeBand()//x(d[d.length-1].year+1) - x(d[0].year);
     })
     .attr('height', y.rangeBand())
-    /*
-    .style('fill', function(d,i) {
-      if (i%2==0) {
-        return '#ddd'
-      } else {
-        return '#e8e8e8'
-      }
-    })
-    */
-    .style('stroke', '#eee')
+    //.style('stroke', '#eee')
 
     //FIXME : write the rank of teams temporarily
     var rank = col.selectAll('text.jg-rank-text')
@@ -177,11 +152,20 @@ d3.kblHistory = function module () {
 
     //TODO : draw year-blocks 1.draw winning rates 2.draw score avg.
 
-
     //TODO :
 
   }
 
+  function selectCols(d,i) {
+    if (d3.select(this).classed('filtered')) {
+      svg.selectAll('.jg-col').classed({'filtered':false})
+    } else {
+      var thisCoach =d[0].first_coach_name
+      svg.selectAll('.jg-col').classed({'filtered':false})
+      svg.selectAll('.jg-col').filter(function(d) {return d[0].first_coach_name === thisCoach})
+        .classed({'filtered':true})
+    }
+  }
 
   function nestFunc(key1, key2 ) {
     return d3.nest()
