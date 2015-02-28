e279d54f904e4559239d1434d8bbe04ad11cd6af
Inho Won <wonderino@gmail.com>
2015-02-28 13:41:28 +0900

    draw rects for proto-typing

--- kbl-history.js
+++ kbl-history.js
@@ -20,7 +20,6 @@ d3.kblHistoryDataMng = function () {
             d['first_coach_name'] = d[k][0]
           }
         }
-
       })
       data = _res;
       dispatch.dataReady(_res);
@@ -43,8 +42,9 @@ d3.kblHistory = function module () {
     isTeamMode : true,
     blockColExtent : [],
   };//end of attributes
-  var margin = {top:20, right : 20, bottom : 10, left : 40}
-  var x = d3.scale.linear(), y=d3.scale.ordinal(), xAxis = d3.svg.axis();
+  var margin = {top:20, right : 10, bottom : 10, left : 50}
+  var x = d3.scale.ordinal(), y=d3.scale.ordinal(), xAxis = d3.svg.axis();
+  var wa = d3.scale.linear();
   var teamMap = d3.map({'OB':'OB','SS':'삼성','MB':'MBC',
   'HT':'해태','LT':'롯데','SM':'삼미','LG':'LG','DS':'두산','KA':'KIA',
   'BG':'빙그레','HH':'한화','SK':'SK','NS':'넥센','NC':'NC','SB':'쌍방울',
@@ -57,18 +57,25 @@ d3.kblHistory = function module () {
       var width = attrs.canvasWidth - margin.left - margin.right;
       var height =  attrs.canvasHeight - margin.top - margin.bottom;
       var yearExtent = d3.extent(_data, function(d) {return d.year})
+      var waExtent = d3.extent(_data, function(d) {return d.wa})
       var nestedByTeam = nestFunc('final_team_code', 'first_coach_name')
       .entries(_data);
       var nestedByCoach = nestFunc('first_coach_name', 'final_team_code')
       .entries(_data);
 
       curData = nestedByTeam;
-      x.rangeRound([0, width]).domain([yearExtent[0], yearExtent[1]+1])
+      x.rangeRoundBands([0, width])
+        .domain(d3.range(yearExtent[0], yearExtent[1]+1))
       y.rangeRoundBands([0, height]).domain(curData.map(function(d) {return d.key}))
 
-      xAxis.tickSize(4)
-      .ticks(20)
-      .tickFormat(d3.format('d'))
+      //FIXME : get the scale of  winning rates
+      xAxis.tickSize(0)
+      //.ticks(20)
+      //.tickValues(x.domain().filter(function(d) {return (d%2===0)}))
+      .tickFormat(function(d) {
+        return d3.format('02d')(d%100) + "'"
+        //d3.format('d'))
+      })
       .orient("top")
       .scale(x)
 
@@ -86,7 +93,6 @@ d3.kblHistory = function module () {
   } // end of exports
 
   function svgInit(svg) {
-
     var labelCol = svg.append('g')
     .attr('transfrom', d3.svg.transform().translate([0, margin.top]))
 
@@ -95,12 +101,11 @@ d3.kblHistory = function module () {
     .enter().append('text')
     .attr('class','jg-team-label')
     .attr('text-anchor', 'end')
-    .attr('x', margin.left*.75)
+    .attr('x', margin.left*.95)
     .attr('y', function(d){return y(d) + y.rangeBand()*.5 + margin.top })
     .attr('dy', '.35em')
     .text(function(d) {return teamMap.get(d)})
 
-    //TODO: draw yearAxis
     svg.append("g")
     .attr("class", "x axis")
     .attr("transform", d3.svg.transform().translate([margin.left, margin.top]))
@@ -124,21 +129,59 @@ d3.kblHistory = function module () {
     .enter().append('g')
     .attr('class', 'jg-col')
     .attr('transform', d3.svg.transform().translate(function(d) {return [x(d[0].year), 0]}))
+    .on('click', function(d,i) {
+      if (d3.select(this).classed('filtered')) {
+
+        col.classed({'filtered':false})
+      } else {
+        var thisCoach =d[0].first_coach_name
+        col.classed({'filtered':false})
+
+        col.filter(function(d) {
+              return d[0].first_coach_name === thisCoach
+          })
+          //.selectAll('rect.jg-col-block')
+          .classed({'filtered':true})
+      }
+    })
 
     col.append('rect')
     .attr('class', 'jg-col-block')
     .attr('x', 0)
     .attr('y', 0)
     .attr('width', function(d) {
-      return x(d[d.length-1].year+1) - x(d[0].year);
+      return d.length * x.rangeBand()//x(d[d.length-1].year+1) - x(d[0].year);
     })
     .attr('height', y.rangeBand())
-    .style('fill', 'none')
-    .style('stroke', '#111')
+    /*
+    .style('fill', function(d,i) {
+      if (i%2==0) {
+        return '#ddd'
+      } else {
+        return '#e8e8e8'
+      }
+    })
+    */
+    .style('stroke', '#eee')
 
-    //TODO: draw year-block
+    //FIXME : write the rank of teams temporarily
+    var rank = col.selectAll('text.jg-rank-text')
+        .data(function(d){return d;})
+      .enter().append('text')
+      .attr('class', 'jg-rank-text')
+      .attr('x', function(d,i) { return i*x.rangeBand() + x.rangeBand()*.5})
+      .attr('y', function(d,i) {return y.rangeBand()*.5})
+      .attr('dy', '.35em')
+      .attr('text-anchor', 'middle')
+      .text(function(d) {return d.rank})
+
+    //TODO : draw year-blocks 1.draw winning rates 2.draw score avg.
+
+
+    //TODO :
 
   }
+
 
   function nestFunc(key1, key2 ) {
     return d3.nest()
