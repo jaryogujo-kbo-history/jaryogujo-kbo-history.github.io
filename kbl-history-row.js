19b2f0b4bfab81cf316ed531d8604b6310a007c9
Inho Won <wonderino@gmail.com>
2015-06-14 10:10:41 +0900

    row 모듈화 시

---
+++ kbl-history-row.js
@@ -0,0 +1,20 @@
+d3.kblHistoryRow = function module () {
+  var attrs = {
+
+  }
+  var dispatch = d3.dispatch("load", "select"); //select.over select.click
+  /*
+  dispatch.load(value);
+  */
+
+  //디스패치 설정
+  var exports = function(_selection) {
+    _selection.each(function(_data) {
+
+    })
+  }
+  //그리기
+
+  //이벤트 동작
+  return exports;
+}
